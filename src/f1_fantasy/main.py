3e4f1dc7a2ea39d305a5bc65c24c0bd6c474888e
Peter <peterHoburg@users.noreply.github.com>
2024-03-01 19:00:53 -0500

    Added some tests

--- src/f1_fantasy/main.py
+++ src/f1_fantasy/main.py
@@ -7,12 +7,21 @@ class Driver:
     def __init__(self, name, price):
         self.name = name
         self.price = price
-        self.points = 0
+        self._points = 0
         self.fastest_lap = False
         self.driver_of_the_day = False
 
         self.qualifying_position: int | None = None
         self.race_position: int | None = None
+
+    @property
+    def points(self):
+        return self._points
+
+    @points.setter
+    def points(self, value: int):
+        self._points = value
+
 
     @property
     def qualifying_position(self):
@@ -33,33 +42,34 @@ class Driver:
         self._race_position = value
 
     def compute_points(self):
+        self._points = 0
         self._compute_qualifying_points()
         self._compute_race_points()
         self._compute_race_position_points()
 
     def _compute_qualifying_points(self):
         if self.qualifying_position is not None and self.qualifying_position <= 10:
-            self.points += qualifying_place_points[self.qualifying_position - 1]
+            self._points += qualifying_place_points[self.qualifying_position - 1]
         elif self.qualifying_position is None:
-            self.points -= 20
+            self._points -= 20
 
     def _compute_race_points(self):
         if self.race_position is not None and self.race_position <= 10:
-            self.points += race_place_points[self.race_position - 1]
+            self._points += race_place_points[self.race_position - 1]
             if self.fastest_lap:
-                self.points += 10
+                self._points += 10
         if self.driver_of_the_day:
-            self.points += 10
+            self._points += 10
 
     def _compute_race_position_points(self):
         position_delta = self.qualifying_position - self.race_position
         if position_delta > 0:
-            self.points += position_delta * 2
+            self._points += position_delta * 2
         elif position_delta < 0:
-            self.points += position_delta
+            self._points += position_delta
 
     def __repr__(self):
-        return f"{self.name} - {self.points} points"
+        return f"{self.name} - {self._points} points"
 
 
 class Constructor:
@@ -67,23 +77,33 @@ class Constructor:
         self.name = name
         self.price = price
         self.drivers: list[Driver] = drivers
-        self.points = 0
+        self._points = 0
         self.fastest_pitstop = False
         self.second_fastest_pitstop = False
         self.third_fastest_pitstop = False
 
+    @property
+    def points(self):
+        return self._points
+
+    @points.setter
+    def points(self, value: int):
+        self._points = value
+
     def compute_points(self):
+        self._points = 0
         for driver in self.drivers:
-            self.points += driver.points
+            driver.compute_points()
+            self._points += driver.points
             if driver.driver_of_the_day is True:
-                self.points -= 10  # constructors do not get driver of the day bonuses
+                self._points -= 10  # constructors do not get driver of the day bonuses
 
         if self.fastest_pitstop:
-            self.points += 10
+            self._points += 10
         if self.second_fastest_pitstop:
-            self.points += 5
+            self._points += 5
         if self.third_fastest_pitstop:
-            self.points += 3
+            self._points += 3
 
 
 MAX = Driver(name="Max Verstappen", price=30.0)
--- tests/test_constructors.py
+++ tests/test_constructors.py
@@ -1,1 +1,55 @@
+from f1_fantasy.main import *
+
+
+def test_driver_of_the_day():
+    MAX.qualifying_position = 1
+    MAX.race_position = 1
+    MAX.fastest_lap = False
+    MAX.driver_of_the_day = True
+
+    SERGIO.qualifying_position = 20
+    SERGIO.race_position = 20
+    SERGIO.fastest_lap = False
+    SERGIO.driver_of_the_day = False
+
+    RED_BULL.compute_points()
+    assert MAX.points == 45
+    assert RED_BULL.points == 35
+
+
+def test_fastest_lap():
+    MAX.qualifying_position = 1
+    MAX.race_position = 1
+    MAX.fastest_lap = True
+    MAX.driver_of_the_day = False
+
+    SERGIO.qualifying_position = 20
+    SERGIO.race_position = 20
+    SERGIO.fastest_lap = False
+    SERGIO.driver_of_the_day = False
+
+    RED_BULL.compute_points()
+    assert MAX.points == 45
+    assert RED_BULL.points == 45
+
+
+def test_fastest_pitstops():
+    MAX.qualifying_position = 19
+    MAX.race_position = 19
+    MAX.fastest_lap = False
+    MAX.driver_of_the_day = False
+
+    SERGIO.qualifying_position = 20
+    SERGIO.race_position = 20
+    SERGIO.fastest_lap = False
+    SERGIO.driver_of_the_day = False
+
+    RED_BULL.fastest_pitstop = True
+    RED_BULL.second_fastest_pitstop = True
+    RED_BULL.third_fastest_pitstop = True
+
+    RED_BULL.compute_points()
+    assert MAX.points == 0
+    assert SERGIO.points == 0
+    assert RED_BULL.points == 18
 
