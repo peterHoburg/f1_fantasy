939f366175abd3cc5931c55cb279419cd0a21d03
Peter <peterHoburg@users.noreply.github.com>
2024-03-01 19:12:56 -0500

    Added positions

--- src/f1_fantasy/main.py
+++ src/f1_fantasy/main.py
@@ -22,7 +22,6 @@ class Driver:
     def points(self, value: int):
         self._points = value
 
-
     @property
     def qualifying_position(self):
         return self._qualifying_position
@@ -39,7 +38,7 @@ class Driver:
     @race_position.setter
     def race_position(self, value: int):
         assert value is None or 1 <= value <= 20, "Race position must be between 1 and 20"
-        self._race_position = value
+        self._race_position = self._qualifying_position
 
     def compute_points(self):
         self._points = 0
@@ -186,217 +185,118 @@ HAAS = Constructor(
     drivers=[NICO, KEVIN],
 )
 
-#
-# def get_constructors():
-#     constructors = [
-#         Constructor(
-#             name="Red Bull Racing",
-#             price=27.9,
-#             drivers=[
-#                 Driver(name="Max Verstappen", team="Red Bull Racing", price=30.0),
-#                 Driver(name="Sergio Perez", team="Red Bull Racing", price=20.8),
-#             ],
-#         ),
-#         Constructor(
-#             name="McLaren",
-#             price=23.2,
-#             drivers=[
-#                 Driver(name="Lando Norris", team="McLaren", price=23.0),
-#                 Driver(name="Oscar Piastri", team="McLaren", price=19.0),
-#             ],
-#         ),
-#         Constructor(
-#             name="Mercedes",
-#             price=20.1,
-#             drivers=[
-#                 Driver(name="Lewis Hamilton", team="Mercedes", price=19.3),
-#                 Driver(name="George Russell", team="Mercedes", price=18.8),
-#             ],
-#         ),
-#         Constructor(
-#             name="Ferrari",
-#             price=19.3,
-#             drivers=[
-#                 Driver(name="Charles Leclerc", team="Ferrari", price=19.1),
-#                 Driver(name="Carlos Sainz", team="Ferrari", price=18.5),
-#             ],
-#         ),
-#         Constructor(
-#             name="Aston Martin",
-#             price=13.6,
-#             drivers=[
-#                 Driver(name="Fernando Alonso", team="Aston Martin", price=15.8),
-#                 Driver(name="Lance Stroll", team="Aston Martin", price=10.7),
-#             ],
-#         ),
-#         Constructor(
-#             name="RB",
-#             price=8.5,
-#             drivers=[
-#                 Driver(name="Daniel Ricciardo", team="RB", price=9.0),
-#                 Driver(name="Yuji Tsunoda", team="RB", price=8.0),
-#             ],
-#         ),
-#         Constructor(
-#             name="Alpine",
-#             price=8.4,
-#             drivers=[
-#                 Driver(name="Pierre Gasly", team="Alpine", price=7.8),
-#                 Driver(name="Esteban Ocon", team="Alpine", price=7.8),
-#             ],
-#         ),
-#         Constructor(
-#             name="Williams",
-#             price=6.3,
-#             drivers=[
-#                 Driver(name="Alexander Albon", team="Williams", price=7.0),
-#                 Driver(name="Logan Sargeant", team="Williams", price=5.5),
-#             ],
-#         ),
-#         Constructor(
-#             name="Sauber",
-#             price=6.6,
-#             drivers=[
-#                 Driver(name="Zhou Guanyu", team="Sauber", price=6.6),
-#                 Driver(name="Valtteri Bottas", team="Sauber", price=6.4),
-#             ],
-#         ),
-#         Constructor(
-#             name="Haas",
-#             price=6.3,
-#             drivers=[
-#                 Driver(name="Nico Hulkenberg", team="Haas", price=6.4),
-#                 Driver(name="Kevin Magnussen", team="Haas", price=6.2),
-#             ],
-#         ),
-#     ]
-#     return constructors
-#
-#
-# def get_driver_quali_order():
-#     drivers = [
-#         DRIVERS.max,
-#         DRIVERS.charles,
-#         DRIVERS.george,
-#         DRIVERS.carlos,
-#         DRIVERS.sergio,
-#         DRIVERS.fernando,
-#         DRIVERS.lando,
-#         DRIVERS.oscar,
-#         DRIVERS.lewis,
-#         DRIVERS.nico,
-#
-#         DRIVERS.yuki,
-#         DRIVERS.lance,
-#         DRIVERS.alexander,
-#         DRIVERS.daniel,
-#         DRIVERS.kevin,
-#
-#         DRIVERS.valtteri,
-#         DRIVERS.zhou,
-#         DRIVERS.logan,
-#         DRIVERS.esteban,
-#         DRIVERS.pierre,
-#     ]
-#     return drivers
-#
-#
-# def get_driver_race_order():
-#     DRIVERS.max.fastest_lap = True
-#     DRIVERS.max.driver_of_the_day = True
-#     drivers = [
-#         DRIVERS.max,
-#         DRIVERS.charles,
-#         DRIVERS.george,
-#
-#         DRIVERS.sergio,  # ++
-#         DRIVERS.carlos,
-#
-#         DRIVERS.lando,  # ++
-#         DRIVERS.fernando,
-#
-#         DRIVERS.lewis,  # ++
-#         DRIVERS.oscar,
-#
-#         DRIVERS.nico,
-#
-#         DRIVERS.yuki,
-#         DRIVERS.lance,
-#         DRIVERS.alexander,
-#         DRIVERS.daniel,
-#         DRIVERS.kevin,
-#         DRIVERS.valtteri,
-#         DRIVERS.zhou,
-#         DRIVERS.logan,
-#         DRIVERS.esteban,
-#         DRIVERS.pierre,
-#     ]
-#     return drivers
-#
-#
-# def compute_quali_points(driver_quali_order):
-#     for position, driver in enumerate(driver_quali_order):
-#         if position < 10:
-#             driver.points = 10 - position
-#
-#
-# def compute_race_points(driver_quali_order, driver_race_order):
-#     for position_quali, driver_quali in enumerate(driver_quali_order):
-#         for position_race, driver_race in enumerate(driver_race_order):
-#             if driver_quali.name == driver_race.name:
-#                 if position_race < 10:
-#                     driver_race.points = driver_race.points + race_place_points[position_race]
-#                 position_delta = position_quali - position_race
-#                 if position_delta > 0:
-#                     driver_race.points = driver_race.points + position_delta * 2
-#                 elif position_delta < 0:
-#                     driver_race.points = driver_race.points + position_delta
-#                 print(position_delta)
-#                 break
-#
-#
-# def compute_fastest_lap_points(driver_race_order):
-#     for position_race, driver_race in enumerate(driver_race_order):
-#         if position_race < 10 and driver_race.fastest_lap is True:
-#             driver_race.points = driver_race.points + 10
-#             return
-#
-# def compute_driver_of_the_day_points(driver_race_order):
-#     for position_race, driver_race in enumerate(driver_race_order):
-#         if driver_race.driver_of_the_day is True:
-#             driver_race.points = driver_race.points + 10
-#             return
-#
-# def compute_points(driver_quali_order, driver_race_order):
-#     compute_quali_points(driver_quali_order)
-#     compute_race_points(driver_quali_order, driver_race_order)
-#
-#     compute_fastest_lap_points(driver_race_order)
-#     compute_driver_of_the_day_points(driver_race_order)
-#
-#
-# def compute_constructor_points():
-#     for _, constructor in CONSTRUCTORS:
-#         for driver in constructor.drivers:
-#             constructor.points = constructor.points + driver.points
-#         if constructor.fastest_pitstop:
-#             constructor.points = constructor.points + 10
-#         if constructor.second_fastest_pitstop:
-#             constructor.points = constructor.points + 5
-#         if constructor.third_fastest_pitstop:
-#             constructor.points = constructor.points + 3
-#
-#
-#
-#
-# def main():
-#     driver_quali_order = get_driver_quali_order()
-#     driver_race_order = get_driver_race_order()
-#     compute_points(driver_quali_order, driver_race_order)
-#     compute_constructor_points()
-#     # print(DRIVERS)
-#     print(CONSTRUCTORS)
-#
-#
-# if __name__ == "__main__":
-#     main()
+ALL_DRIVERS = [
+    MAX,
+    SERGIO,
+    LANDO,
+    OSCAR,
+    LEWIS,
+    GEORGE,
+    CHARLES,
+    CARLOS,
+    FERNANDO,
+    LANCE,
+    DANIEL,
+    YUKI,
+    PIERRE,
+    ESTEBAN,
+    ALEXANDER,
+    LOGAN,
+    ZHOU,
+    VALTTERI,
+    NICO,
+    KEVIN,
+]
+
+ALL_CONSTRUCTORS = [
+    RED_BULL,
+    MCLAREN,
+    MERCEDES,
+    FERRARI,
+    ASTON_MARTIN,
+    RB,
+    ALPINE,
+    WILLIAMS,
+    SAUBER,
+    HAAS,
+]
+
+def check_drivers_qualifying_positions():
+    used_positions = set()
+    for driver in ALL_DRIVERS:
+        if driver.qualifying_position is None:
+            continue
+        if driver.qualifying_position in used_positions:
+            print(f"Position {driver.qualifying_position} is already used by another driver")
+            raise ValueError
+        else:
+            used_positions.add(driver.qualifying_position)
+
+def check_drivers_race_positions():
+    used_positions = set()
+    for driver in ALL_DRIVERS:
+        if driver.race_position is None:
+            continue
+        if driver.race_position in used_positions:
+            print(f"Position {driver.race_position} is already used by another driver")
+            raise ValueError
+        else:
+            used_positions.add(driver.race_position)
+
+
+def set_drivers_qualifying_positions():
+    MAX.qualifying_position = 1
+    CHARLES.qualifying_position = 2
+    GEORGE.qualifying_position = 3
+    CARLOS.qualifying_position = 4
+    SERGIO.qualifying_position = 5
+    FERNANDO.qualifying_position = 6
+    LANDO.qualifying_position = 7
+    OSCAR.qualifying_position = 8
+    LEWIS.qualifying_position = 9
+    NICO.qualifying_position = 10
+
+    YUKI.qualifying_position = 11
+    LANCE.qualifying_position = 12
+    ALEXANDER.qualifying_position = 13
+    DANIEL.qualifying_position = 14
+    KEVIN.qualifying_position = 15
+    VALTTERI.qualifying_position = 16
+    ZHOU.qualifying_position = 17
+    ESTEBAN.qualifying_position = 18
+    PIERRE.qualifying_position = 19
+
+
+def set_drivers_race_positions():
+    MAX.race_position = MAX.qualifying_position
+    CHARLES.race_position = CHARLES.qualifying_position
+    GEORGE.race_position = GEORGE.qualifying_position - 2
+    CARLOS.race_position = CARLOS.qualifying_position + 1
+    SERGIO.race_position = SERGIO.qualifying_position + 1
+    FERNANDO.race_position = FERNANDO.qualifying_position
+    LANDO.race_position = LANDO.qualifying_position
+    OSCAR.race_position = OSCAR.qualifying_position - 1
+    LEWIS.race_position = LEWIS.qualifying_position + 1
+    NICO.race_position = NICO.qualifying_position
+
+    YUKI.race_position = YUKI.qualifying_position
+    LANCE.race_position = LANCE.qualifying_position
+    ALEXANDER.race_position = ALEXANDER.qualifying_position
+    DANIEL.race_position = DANIEL.qualifying_position
+    KEVIN.race_position = KEVIN.qualifying_position
+    VALTTERI.race_position = VALTTERI.qualifying_position
+    ZHOU.race_position = ZHOU.qualifying_position
+    ESTEBAN.race_position = ESTEBAN.qualifying_position
+    PIERRE.race_position = PIERRE.qualifying_position
+
+
+def main():
+    set_drivers_qualifying_positions()
+    check_drivers_qualifying_positions()
+    set_drivers_race_positions()
+    check_drivers_race_positions()
+
+
+
+if __name__ == "__main__":
+    main()
