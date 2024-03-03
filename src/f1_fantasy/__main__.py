0eaa880643000ac4f6cba735ab8442544474de5d
Peter <peterHoburg@users.noreply.github.com>
2024-03-03 11:12:24 -0500

    Formatting

--- src/f1_fantasy/__main__.py
+++ src/f1_fantasy/__main__.py
@@ -2,15 +2,16 @@ import shutil
 from pathlib import Path
 
 import typer
-from f1_fantasy.main import main
+
 from f1_fantasy.consts import ROOT_DIR
+from f1_fantasy.main import main
 
 app = typer.Typer()
 
 
 @app.command()
 def setup():
-    current_dir = Path(".") / "data"
+    current_dir = Path() / "data"
     typer.echo(f"Copying data to {current_dir}")
     shutil.copytree(ROOT_DIR / "data", current_dir)
 
@@ -18,7 +19,6 @@ def setup():
 @app.command()
 def run():
     main()
-
 
 
 if __name__ == "__main__":
